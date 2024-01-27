84dfb191fef47bd5ac18ec78a81ea2659be7023c
Wizzard2001 <rpossberg1@nait.ca>
2024-01-26 19:25:34 -0700

    Add script tag for index.js and create reference variables

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
- 
+console.log("Hello from index.js");
+
+// Task: Reference Variables
+
+/*
+    Create reference variables for the following:
+    Call to action button, the dialog element and the close button inside the dialog.
+*/
+
+const openDialogButton = document.querySelector("#open-modal");
+
+const speedTest = document.querySelector("#speed-test");
+
+const closeButton = document.querySelector("#close-modal");
