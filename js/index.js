545ac4d11fe0aa87357569b40dc9424cc84884b6
Wizzard2001 <rpossberg1@nait.ca>
2024-01-26 20:07:45 -0700

    Add event listeners and functions for opening and closing the dialog

--- js/index.js
+++ js/index.js
@@ -4,11 +4,27 @@ console.log("Hello from index.js");
 
 /*
     Create reference variables for the following:
-    Call to action button, the dialog element and the close button inside the dialog.
+    Call to action button (speed-test), the dialog element and the close button inside the dialog.
 */
 
 const openDialogButton = document.querySelector("#open-modal");
-
 const speedTest = document.querySelector("#speed-test");
-
 const closeButton = document.querySelector("#close-modal");
+
+// Task: Add Event Listeners
+// Add an event listener to the call to action button that will open the dialog.
+openDialogButton.addEventListener("click", onUpdateDialog);
+
+// Add an event listener to the close button that will close the dialog.
+closeButton.addEventListener("click", onCloseDialog);
+
+// Task: Create Functions
+// Create a function that will open the dialog.
+function onUpdateDialog(e) {
+  speedTest.showModal();
+}
+
+// Create a function that will close the dialog.
+function onCloseDialog(e) {
+  speedTest.close();
+}
